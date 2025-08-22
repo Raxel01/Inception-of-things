60b612dc407a55b4083b022139fa63a8b11de930
Abdelali Ait Talb <abait-ta@e3r2p12.1337.ma>
2025-08-22 17:25:25 +0100

    feature : implementing extracting master token

--- p1/Scripts/agent_provision.sh
+++ p1/Scripts/agent_provision.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-CONTROLLER_URL = "http://$1:6443"
+# CONTROLLER_URL = "http://$1:6443"
 
-# get the token from the machine of the controller
-MY_K3S_TOKEN = ${curl -s $MY_K3S_URL}
+# # get the token from the machine of the controller
+# MY_K3S_TOKEN = ${curl -s $MY_K3S_URL}
 
-# and then run this line: 
-curl -sfL https://get.k3s.io | K3S_URL=$CONTROLLER_URL K3S_TOKEN=$MY_K3S_TOKEN sh -
+# # and then run this line: 
+# curl -sfL https://get.k3s.io | K3S_URL=$CONTROLLER_URL K3S_TOKEN=$MY_K3S_TOKEN sh -
 
-sudo systemctl enable k3s-agent
-sudo systemctl start k3s-agent
+# sudo systemctl enable k3s-agent
+# sudo systemctl start k3s-agent
 
-echo "========= K3s Agent joined the cluster ========"
+# echo "========= K3s Agent joined the cluster ========"
 echo "############# End Agent Provision #############"
--- p1/Scripts/controller_provision.sh
+++ p1/Scripts/controller_provision.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -
 
+sudo usermod -aG sudo vagrant
+# # sudo cat /var/lib/rancher/k3s/server/node-token
+# sudo k3s kubectl get nodes >> installation.txt
+
+# cat installation.txt | grep "control-place"
+
+# if ["$?" != "0"];then
+#     exit 127
+# fi
+# this will 100% confirm that the k3s controller is well working
+echo "======== K3s Controller is UP ========"
 echo "############# End Controller Provision #############"
