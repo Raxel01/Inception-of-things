820efc80f7e79494a1779aa5294145dfa86363d4
abait-ta <abait-ta@iot-42paris.myguest.virtualbox.org>
2025-10-26 12:11:10 +0100

    feature: version Tested inside VM
    remove vagrantfile for p3 as it's not required
    
    add apply argocd-configs.yaml

--- p3/scripts/init_infrastructure.sh
+++ p3/scripts/init_infrastructure.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# add  vagrant user to docker group
-sudo usermod -aG sudo $USER
-
-k3d cluster create part-three-cluster -p "30443:30443@server:0"
+sudo k3d cluster create part-three-cluster -p "30443:30443@server:0"
 
 cd /vagrant
 
 sudo kubectl create -f confs/namespaces.yaml
 
-kubectl apply  -f confs/go-app-deployment.yaml -n dev
+sudo kubectl apply  -f confs/go-app-deployment.yaml -n dev
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml 
 
 sudo kubectl apply -f confs/argocdNodeportService.yaml -n argocd
+
+sudo kubectl apply -f confs/argocd-config.yaml 
